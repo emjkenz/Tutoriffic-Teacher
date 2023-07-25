1b051e24a9f980f67169ee10fdd0ecc2ae2231ce
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-07-25 20:31:00 +1000

    created client side quiz creator

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+
+import QuizCreator from './pages/QuizCreator';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -14,28 +12,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />}
-            />
-            <Route 
-              path="/matchup" 
-              element={<Matchup />}
-            />
-            <Route 
-              path="/matchup/:id" 
-              element={<Vote />}
-            />
-            <Route 
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
+              <div>
+                <QuizCreator />           
         </div>
-      </Router>
     </ApolloProvider>
   );
 }
