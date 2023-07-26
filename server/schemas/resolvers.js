91a011c8b91661068161fb1519ce7c409336ff80
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-26 17:27:14 +1000

    Added resolver queries for get all quizzes and get quiz by id

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,25 @@
 const { Quiz, Questions } = require('../models');
 
 const resolvers = {
+  Query: {
+    quizzes: async () => {
+
+      return await Quiz.find();
+    },
+    quiz: async (parent, { id }) => {
+
+      const foundQuiz = await Quiz.findOne({id: id});
+
+      if (!foundQuiz) {
+        throw new Error('Cannot find a quiz with this id!');
+      }
+
+      return foundQuiz;
+    }
+  },
   Mutation: {
     saveQuiz: async (parent, {quizData}) => {
       const { id, title, description, questions } = quizData;
-
-      console.log(id);
-      console.log(title);
-      console.log(description);
-      console.log(questions);
 
       return await Quiz.create({
         id,
