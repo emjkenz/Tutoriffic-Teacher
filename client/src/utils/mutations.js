d2189fe0e2e62c2c632b2b52604e2e733b6571fd
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-26 14:52:51 +1000

    front end now saves data to the database using the saveBook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,15 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const SAVE_QUIZ = gql`
+  mutation saveQuiz($quizData: QuizInput!) {
+    saveQuiz(quizData: $quizData) {
+      id
+      title
+      description
+      questions {
+        question
+        answers
+      }
     }
   }
 `;
